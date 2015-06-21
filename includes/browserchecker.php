4a797302dd7aa1128b4a5ed8a551c36cc3dfd94e
wurfmaul <wurfmaul@posteo.at>
2015-06-21 16:15:44 +0200

    Browser check: added Safari

--- includes/browserchecker.php
+++ includes/browserchecker.php
@@ -2,20 +2,26 @@
 class BrowserChecker {
     public static function isUnsupported() {
         $_agent = $_SERVER['HTTP_USER_AGENT'];
-
-        // check for old internet explorers
+        // check for Microsoft Internet Explorer
         if (preg_match('/MSIE (.*?);/', $_agent, $matches)) {
-            // IE < 9.0 are not supported by jquery and uglify
             if (version_compare($matches[1], '9.0', '<')) {
+                // IE < 9.0 are not supported by jquery and uglify
                 return "Microsoft Internet Explorer &leq; 8.0";
             }
         }
+        // check for Apple Safari
+        if (preg_match('/AppleWebKit.*Version\/(.*?) /', $_agent, $matches)) {
+            if (preg_match('/Windows/', $_agent)) {
+                // Safari for Windows is not supported by bootstrap
+                return "Safari for Windows";
+            } elseif (version_compare($matches[1], '5.1', '<')) {
+                // Safari < 5.1 is not supported by jquery
+                return "Safari &leq; 5.1";
+            }
+        }
         /** TODO: check browsers
-         * - Safari on Windows (bootstrap)
-         * - Safari 5.1+ (jquery ui)
          * - iOS 6.1+ (jquery ui)
          * - Android 4.0+ (jquery ui)
-         *
          */
         return false;
     }
