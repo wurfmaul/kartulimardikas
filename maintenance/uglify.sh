3c90e7f59eae1458aed46f10b6a35b815aa7af58
wurfmaul <wurfmaul@posteo.at>
2015-07-05 19:27:35 +0200

    Uglify fixed

--- maintenance/uglify.sh
+++ maintenance/uglify.sh
@@ -6,7 +6,7 @@ UGLIFY_JS_OPTIONS="-c -m --screw-ie8 --comments"
 /usr/bin/uglifyjs ${JS_PATH}/{common,admin,table}.js ${UGLIFY_JS_OPTIONS} -o ${JS_PATH}/admin.min.js
 /usr/bin/uglifyjs ${JS_PATH}/{common,section,autocomplete,algorithm,edit}.js ${UGLIFY_JS_OPTIONS} -o ${JS_PATH}/edit.min.js
 /usr/bin/uglifyjs ${JS_PATH}/common.js ${UGLIFY_JS_OPTIONS} -o ${JS_PATH}/home.min.js
-/usr/bin/uglifyjs ${JS_PATH}/common.js ${UGLIFY_JS_OPTIONS} -o ${JS_PATH}/index.min.js
+/usr/bin/uglifyjs ${JS_PATH}/{common,table}.js ${UGLIFY_JS_OPTIONS} -o ${JS_PATH}/index.min.js
 /usr/bin/uglifyjs ${JS_PATH}/common.js ${UGLIFY_JS_OPTIONS} -o ${JS_PATH}/notice.min.js
 /usr/bin/uglifyjs ${JS_PATH}/{common,register}.js ${UGLIFY_JS_OPTIONS} -o ${JS_PATH}/register.min.js
 /usr/bin/uglifyjs ${JS_PATH}/{common,settings}.js ${UGLIFY_JS_OPTIONS} -o ${JS_PATH}/settings.min.js
@@ -16,7 +16,7 @@ UGLIFY_JS_OPTIONS="-c -m --screw-ie8 --comments"
 ### Use uglify-css (https://www.npmjs.com/package/uglifycss) to compress css files. ###
 CSS_PATH=../css
 UGLIFY_CSS_OPTIONS="--cute-comments"
-/usr/bin/uglifycss ${UGLIFY_CSS_OPTIONS} ${CSS_PATH}/common.css > ${CSS_PATH}/admin.min.css
+/usr/bin/uglifycss ${UGLIFY_CSS_OPTIONS} ${CSS_PATH}/{common,admin}.css > ${CSS_PATH}/admin.min.css
 /usr/bin/uglifycss ${UGLIFY_CSS_OPTIONS} ${CSS_PATH}/{common,edit}.css > ${CSS_PATH}/edit.min.css
 /usr/bin/uglifycss ${UGLIFY_CSS_OPTIONS} ${CSS_PATH}/common.css > ${CSS_PATH}/home.min.css
 /usr/bin/uglifycss ${UGLIFY_CSS_OPTIONS} ${CSS_PATH}/{common,index}.css > ${CSS_PATH}/index.min.css
