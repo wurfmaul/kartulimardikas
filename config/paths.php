3755ca69330239a82568a9b735db2bf025c2df60
wurfmaul <wurfmaul@posteo.at>
2015-12-13 19:57:12 +0100

    Move minified files, update libraries

--- config/paths.php
+++ config/paths.php
@@ -2,9 +2,9 @@
 # Library paths
 $_jqueryVersion = '2.1.4';
 $_jqueryUiVersion = '1.11.4';
-$_bootstrapVersion = '3.3.5';
-$_fontAwesomeVersion = '4.4.0';
-$_tableSorterVersion = '2.23.5';
+$_bootstrapVersion = '3.3.6';
+$_fontAwesomeVersion = '4.5.0';
+$_tableSorterVersion = '2.24.6';
 
 if (LIBRARY_MODE == 'LOCAL') {
     define('JQUERY_PATH', "lib/jquery/$_jqueryVersion/jquery.min.js");
--- maintenance/uglify.sh
+++ maintenance/uglify.sh
@@ -2,12 +2,13 @@
 
 ### Use uglify-js (https://www.npmjs.com/package/uglify-js) to compress js files. ###
 JS_PATH=../js
-JS_PATH_GEN=../js/gen
+JS_PATH_GEN=../js
 UGLIFY_JS_OPTIONS="-c -m --screw-ie8 --comments"
 uglifyjs ${JS_PATH}/{common,admin,table}.js ${UGLIFY_JS_OPTIONS} -o ${JS_PATH_GEN}/admin.min.js
 uglifyjs ${JS_PATH}/{common,section,autocomplete,value,node,memory,tree,edit}.js ${UGLIFY_JS_OPTIONS} -o ${JS_PATH_GEN}/edit.min.js
 uglifyjs ${JS_PATH}/common.js ${UGLIFY_JS_OPTIONS} -o ${JS_PATH_GEN}/home.min.js
 uglifyjs ${JS_PATH}/{common,table}.js ${UGLIFY_JS_OPTIONS} -o ${JS_PATH_GEN}/index.min.js
+uglifyjs ${JS_PATH}/common.js ${UGLIFY_JS_OPTIONS} -o ${JS_PATH_GEN}/new.min.js
 uglifyjs ${JS_PATH}/common.js ${UGLIFY_JS_OPTIONS} -o ${JS_PATH_GEN}/notice.min.js
 uglifyjs ${JS_PATH}/{common,register}.js ${UGLIFY_JS_OPTIONS} -o ${JS_PATH_GEN}/register.min.js
 uglifyjs ${JS_PATH}/{common,settings}.js ${UGLIFY_JS_OPTIONS} -o ${JS_PATH_GEN}/settings.min.js
@@ -16,12 +17,13 @@ uglifyjs ${JS_PATH}/{common,section,value,node,memory,tree,view}.js ${UGLIFY_JS_
 
 ### Use uglify-css (https://www.npmjs.com/package/uglifycss) to compress css files. ###
 CSS_PATH=../css
-CSS_PATH_GEN=../css/gen
+CSS_PATH_GEN=../css
 UGLIFY_CSS_OPTIONS="--cute-comments"
 uglifycss ${UGLIFY_CSS_OPTIONS} ${CSS_PATH}/{common,admin}.css > ${CSS_PATH_GEN}/admin.min.css
 uglifycss ${UGLIFY_CSS_OPTIONS} ${CSS_PATH}/{common,edit}.css > ${CSS_PATH_GEN}/edit.min.css
 uglifycss ${UGLIFY_CSS_OPTIONS} ${CSS_PATH}/common.css > ${CSS_PATH_GEN}/home.min.css
 uglifycss ${UGLIFY_CSS_OPTIONS} ${CSS_PATH}/{common,index}.css > ${CSS_PATH_GEN}/index.min.css
+uglifycss ${UGLIFY_CSS_OPTIONS} ${CSS_PATH}/common.css > ${CSS_PATH_GEN}/new.min.css
 uglifycss ${UGLIFY_CSS_OPTIONS} ${CSS_PATH}/common.css > ${CSS_PATH_GEN}/notice.min.css
 uglifycss ${UGLIFY_CSS_OPTIONS} ${CSS_PATH}/common.css > ${CSS_PATH_GEN}/register.min.css
 uglifycss ${UGLIFY_CSS_OPTIONS} ${CSS_PATH}/common.css > ${CSS_PATH_GEN}/settings.min.css
